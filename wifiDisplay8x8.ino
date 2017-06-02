030b5a9879416ad6a5cdd2ee60e1bda2fc484c2d
chante42 <chante42@gmail.com>
2017-06-02 23:39:04 +0200

    modified:   CHANGES 	new file:   PAGE_NetworkConfiguration.h 	new file:   Page_Admin.h 	new file:   Page_General.h 	new file:   Page_Information.h 	new file:   Page_NTPSettings.h 	new file:   Page_Root.h 	new file:   Page_Script.js.h 	new file:   Page_Style.css.h 	new file:   example.h 	new file:   global.h 	new file:   helpers.h 	modified:   wifiDisplay8x8.ino
    	- Ajout de la gestion de configuration en mode AP d'après :
    	   https://www.john-lassen.de/en/projects/esp-8266-arduino-ide-webconfig

--- wifiDisplay8x8.ino
+++ wifiDisplay8x8.ino
@@ -17,16 +17,36 @@
 // https://www.john-lassen.de/en/projects/esp-8266-arduino-ide-webconfig
 //
 //
-//#include <WiFiClient.h>
 #include <ESP8266WiFi.h>
-#include <NTPClient.h>
 #include <ESP8266WebServer.h>
+#include <Ticker.h>
+#include <EEPROM.h>
 #include <WiFiUdp.h>
+#include "helpers.h"
+#include "global.h"
 #include <Wire.h>  // This library is already built in to the Arduino IDE
-
 #include <SPI.h>
 #include <bitBangedSPI.h>
 #include "MAX7219_Dot_Matrix.h"
+
+/*
+Include the HTML, STYLE and Script "Pages"
+*/
+#include "Page_Root.h"
+#include "Page_Admin.h"
+#include "Page_Script.js.h"
+#include "Page_Style.css.h"
+#include "Page_NTPsettings.h"
+#include "Page_Information.h"
+#include "Page_General.h"
+#include "PAGE_NetworkConfiguration.h"
+#include "example.h"
+
+
+#define ACCESS_POINT_NAME  "ESP-LED-8x8"        
+#define ACCESS_POINT_PASSWORD  "12345678" 
+#define AdminTimeOut 120  // Defines the Time in Seconds, when the Admin-Mode will be diabled
+
 
 const byte NbMax7219 = 15;
 const char* ssid = "Bbox-0E7760"; // put your router name
@@ -69,13 +89,7 @@ byte GlobalState = STATE_NORMAL;
 #define MAXLENMESSAGE  200
 
 //WiFiServer server(80);
-ESP8266WebServer server(80);
-
-WiFiUDP ntpUDP;
-// 'time.nist.gov' is used with 60 seconds update interval and
-// offset de 2 heures -=- Comment gérée le changement d'heure ???????
-NTPClient timeClient(ntpUDP, 3600*2);
-
+//ESP8266WebServer server(80); dans global.h
 
 char  message [MAXLENMESSAGE] = "Olivier Chanteloup 2017/04/24  eé eè aà";
 
@@ -114,44 +128,91 @@ String getHtmlPageFooter() {
 void setup () {
   display.begin ();
 
-  strncpy(message, "getIP.......", MAXLENMESSAGE - 1);
+  strncpy(message, "http://192.168.4.1/admin.html", MAXLENMESSAGE - 1);
   updateDisplay();
   Serial.begin(9600);
-  delay(100);
-
   // We start by connecting to a WiFi network
 
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  EEPROM.begin(512);
+  delay(500);
+  Serial.println("Starting ES8266");
+  if (!ReadConfig())
+  {
+    // DEFAULT CONFIG
+    config.ssid = "MYSSID";
+    config.password = "MYPASSWORD";
+    config.dhcp = true;
+    config.IP[0] = 192;config.IP[1] = 168;config.IP[2] = 1;config.IP[3] = 100;
+    config.Netmask[0] = 255;config.Netmask[1] = 255;config.Netmask[2] = 255;config.Netmask[3] = 0;
+    config.Gateway[0] = 192;config.Gateway[1] = 168;config.Gateway[2] = 1;config.Gateway[3] = 1;
+    config.ntpServerName = "0.de.pool.ntp.org";
+    config.Update_Time_Via_NTP_Every =  0;
+    config.timezone = -10;
+    config.daylight = true;
+    config.DeviceName = "Not Named";
+    config.AutoTurnOff = false;
+    config.AutoTurnOn = false;
+    config.TurnOffHour = 0;
+    config.TurnOffMinute = 0;
+    config.TurnOnHour = 0;
+    config.TurnOnMinute = 0;
+    WriteConfig();
+    Serial.println("General config applied");
   }
+  
+  
+  if (AdminEnabled)
+  {
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.softAP( ACCESS_POINT_NAME , ACCESS_POINT_PASSWORD);
+  }
+  else
+  {
+    WiFi.mode(WIFI_STA);
+  }
+
+  ConfigureWifi();
+  
+
+  //server.on ( "/", processExample  );
+  server.on("/", handleRootPage);
+  server.on("/submit", handleSubmitPage);
+  
+  server.on ( "/admin/filldynamicdata", filldynamicdata );
+  
+  server.on ( "/favicon.ico",   []() { Serial.println("favicon.ico"); server.send ( 200, "text/html", "" );   }  );
+
+
+  server.on ( "/admin.html", []() { Serial.println("admin.html"); server.send ( 200, "text/html", PAGE_AdminMainPage );   }  );
+  server.on ( "/config.html", send_network_configuration_html );
+  server.on ( "/info.html", []() { Serial.println("info.html"); server.send ( 200, "text/html", PAGE_Information );   }  );
+  server.on ( "/ntp.html", send_NTP_configuration_html  );
+  server.on ( "/general.html", send_general_html  );
+//  server.on ( "/example.html", []() { server.send ( 200, "text/html", PAGE_EXAMPLE );  } );
+  server.on ( "/style.css", []() { Serial.println("style.css"); server.send ( 200, "text/plain", PAGE_Style_css );  } );
+  server.on ( "/microajax.js", []() { Serial.println("microajax.js"); server.send ( 200, "text/plain", PAGE_microajax_js );  } );
+  server.on ( "/admin/values", send_network_configuration_values_html );
+  server.on ( "/admin/connectionstate", send_connection_state_values_html );
+  server.on ( "/admin/infovalues", send_information_values_html );
+  server.on ( "/admin/ntpvalues", send_NTP_configuration_values_html );
+  server.on ( "/admin/generalvalues", send_general_configuration_values_html);
+  server.on ( "/admin/devicename",     send_devicename_value_html);
+ 
+
+ 
+
+  server.onNotFound ( []() { Serial.println("Page Not Found"); server.send ( 400, "text/html", "Page not Found" );   }  );
+  server.begin();
+  Serial.println( "HTTP server started" );
+  tkSecond.attach(1,Second_Tick);
+  UDPNTPClient.begin(2390);  // Port for NTP receive
 
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-  timeClient.begin();
-  timeClient.update();
-
   
-  strncpy(message, "ip: ", MAXLENMESSAGE - 1);
-  strncat(message, WiFi.localIP().toString().c_str(), MAXLENMESSAGE - 1);
-  strncat(message, " ", MAXLENMESSAGE - 1);
-  strncat(message, timeClient.getFormattedTime().c_str(),MAXLENMESSAGE - 1);
-    
-  message[MAXLENMESSAGE - 2] = 0;
-
-  server.on("/", handleRootPage);
-  server.on("/submit", handleSubmitPage);
-  server.begin();
+  
   Serial.printf("Web server started, open %s in a web browser\n", WiFi.localIP().toString().c_str());
 
   // initialise le desteteur de présence
@@ -263,22 +324,78 @@ void handleSubmitPage() {
 
   server.send(200, "text/html", getHtmlPageHeader() + getHtmlPageMiddle() + tmp  + getHtmlPageFooter());
 
-}
+}// FIN handleSubmitPage()
 
 //
 // loop
 //
 void loop () {
 
+  if (AdminEnabled)
+  {
+    if (AdminTimeOutCounter > AdminTimeOut)
+    {
+       AdminEnabled = false;
+       Serial.println("Admin Mode disabled!");
+       WiFi.disconnect();
+       WiFi.softAPdisconnect(true);
+       WiFi.mode(WIFI_STA);
+      WiFi.begin((const char *)config.ssid.c_str(), (const char *)config.password.c_str());
+      Serial.print("connection to: |");
+      Serial.print(config.ssid);
+      Serial.print("|  passwd: |");
+      Serial.print(config.password);
+      Serial.println("|");
+
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+      }
+
+      Serial.println("");
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+    }
+  }
+  if (config.Update_Time_Via_NTP_Every  > 0 )  {
+    if (cNTP_Update > 5 && firstStart)    {
+      NTPRefresh();
+      cNTP_Update =0;
+      firstStart = false;
+    }
+    else if ( cNTP_Update > (config.Update_Time_Via_NTP_Every * 60) )    {
+      NTPRefresh();
+      cNTP_Update =0;
+    }
+  }
+
+  if(DateTime.minute != Minute_Old)  {
+     Minute_Old = DateTime.minute;
+     if (config.AutoTurnOn)     {
+       if (DateTime.hour == config.TurnOnHour && DateTime.minute == config.TurnOnMinute) {
+          Serial.println("SwitchON");
+       }
+     }
+
+
+     Minute_Old = DateTime.minute;
+     if (config.AutoTurnOff)  {
+       if (DateTime.hour == config.TurnOffHour && DateTime.minute == config.TurnOffMinute)   {
+          Serial.println("SwitchOff");
+       }
+     }
+  }
+
   if (GlobalState == STATE_NORMAL) {
     // update display if time is up
     if (millis () - lastMoved >= MOVE_INTERVAL) {
       updateDisplay ();
       lastMoved = millis ();
-      //Serial.println(timeClient.getFormattedTime());
     }
   }
 
+  // gestion du capteur infra rouge, mais ne focntionne pas ?????
   PresenceVal = digitalRead(PresencePin);
   if (PresenceVal == HIGH) {
     //   Serial.println("Hight");
